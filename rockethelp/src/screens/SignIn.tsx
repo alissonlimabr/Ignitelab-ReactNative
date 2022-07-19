98c48d7088163b5809b6af4841a03acf8f791ad7
Alisson Lima <amlxd5@gmail.com>
2022-07-19 17:55:59 -0500

    Adicionado ícones aos inputs + estilização

--- rockethelp/src/screens/SignIn.tsx
+++ rockethelp/src/screens/SignIn.tsx
@@ -5,13 +5,17 @@
 // mt= margin-top
 // mb = margin bottom
 // Heading = Elemento usado para textos em destaque
-import { Heading, Icon, VStack } from 'native-base';
+// useTheme = Hoock que dá acesso ao token de cores do tema do projeto
+import { Heading, Icon, VStack, useTheme } from 'native-base';
 import { Input } from '../components/Input';
 // Importa a imagem svg para ser usado como um component
 import Logo from '../assets/logo_primary.svg';
 import { Envelope, Key } from 'phosphor-react-native';
 
 export function SignIn() {
+
+    // Extrai as cores do tema do projeto para um objeto, por meio do useTheme();
+    const {colors} = useTheme();
 
     return (
         <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -25,9 +29,22 @@ export function SignIn() {
             <Input 
             placeholder="E-mail"
             mb={4}
-            InputLeftElement={<Icon as={<Envelope />} />}
+
+            // cria um elemento à esquerda (ícone). Atribui o modelo (envelope), em seguida, estiliza por meio
+            // da propriedade color, acessa o objeto com todas as cores do tema e seleciona gray[700]
+            // A sintaxe é: colors={objetoHook.cor[numeração definida]}
+            InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} /> } ml={4} />}
             />
-            <Input placeholder="Senha"/>
+
+            <Input 
+            placeholder="Senha"
+            mb={4}
+            InputLeftElement={<Icon as={<Key color={colors.gray[300]} /> } ml={4} />}
+            // Propriedade que oculta os caracteres digitados pelo usuário
+            secureTextEntry
+
+            
+            />
 
         </VStack>
     )
